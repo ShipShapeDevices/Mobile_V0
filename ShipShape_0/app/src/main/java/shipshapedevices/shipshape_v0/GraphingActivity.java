8c52cad12a1320baaa27c2518abccc0e6d48627a
millermw <millermw@ucla.edu>
2017-05-16 20:58:50 -0600

    Initial attempted commit

---
+++ ShipShape_0/app/src/main/java/shipshapedevices/shipshape_v0/GraphingActivity.java
@@ -0,0 +1,13 @@
+package shipshapedevices.shipshape_v0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GraphingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphing);
+    }
+}
